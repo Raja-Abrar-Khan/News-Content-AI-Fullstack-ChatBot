527228b69b83788ffec42f2cce2ac16b52a9e161
Raja Abrar Khan <abrarkhan778008@gmail.com>
2025-01-23 13:55:32 +0530

    comment

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, world!');
 });
+//Server is running
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
